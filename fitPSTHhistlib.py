ec524db72662f853262a1cc8d373d4cadf5b38ec
KensukeArai <kensuke.y.arai@gmail.com>
2014-08-20 22:48:47 -0400

    b4 removing frstSpk from fitPSTHhistlib

--- fitPSTHhistlib.py
+++ fitPSTHhistlib.py
@@ -1,30 +1,27 @@
-
+import numpy as _N
 ##################################
 # Functions for the history term #
 ##################################
 
-def h_L(aS, phiS, M, B, Gm, sts, itvs, TM):
+def h_L(aS, phiS, M, B, Gm, sts, itvs, TM, dt, frstSpk):
     L    = 0
     for m in xrange(M):
         ITVS = len(itvs[m])
-        nSpks= ITVS - 1
 
-        for it in xrange(ITVS):    #  
+        for it in xrange(frstSpk, ITVS):    #  
             i0 = itvs[m][it][0]
             i1 = itvs[m][it][1]
             #  The time integral
             expV = _N.exp(_N.dot(B.T[i0:i1], aS) + _N.dot(Gm.T[0:i1-i0], phiS))
-            L += -dt*_N.sum(expV)
+            L += -dt*_N.sum(expV)  # integration
 
         L += _N.sum(_N.dot(B.T[sts[m][1:]], aS))   #  First spk is fake
-        allISIs = sts[m][1:] - sts[m][0:-1]
+        allISIs = sts[m][frstSpk+1:] - sts[m][frstSpk:-1]
         shrtISIs= allISIs[_N.where(allISIs < TM)[0]]
-        #longISIs= allISIs[_N.where(allISIs >=TM)[0]]
+        print len(shrtISIs)
         L += _N.sum(_N.dot(Gm.T[shrtISIs], phiS))
-        #L += _N.sum(Gm.T[longISIs])   #  for long ones, Gm=0 for all components
 
     return L
-
 
 def h_dL(a_phi, *args):
     nbs1 = args[0]       #  number basis splines PSTH
@@ -37,7 +34,9 @@ def h_dL(a_phi, *args):
     doAl = args[7]       #  fix either PSTH or history term
     doPh = args[8]
     #  history term expressed in terms of splines until time TM
-    TM   = args[9]
+    TM   = args[9]       #  how much of history term expressed by spline
+    dt   = args[10]
+    frstSpk = args[11]   #  Ignore first spike or not.  (0 or 1)
     
     aS   = a_phi[0:nbs1]
     phiS = a_phi[nbs1:]
@@ -50,13 +49,22 @@ def h_dL(a_phi, *args):
     ##  phi COMPONENTS
     for m in xrange(M):
         ITVS = len(itvs[m])
-        nSpks= ITVS - 1
 
-        for it in xrange(ITVS):    #  
+        for it in xrange(frstSpk, ITVS):    #  
             i0 = itvs[m][it][0]
             i1 = itvs[m][it][1]
+            Tunt = i1 - i0
+            if i1 - i0 > TM:
+                Tunt = TM
+
+            gt0= 0      #  
+            if i0 < 1:  #  reference to fake spike
+                gt0 = 0-i0
+            gt1= i1-i0
+            if i0 < 1:  #  reference to fake spike
+                i0 = 0  #  if there was a spike, i0==1
             #  The time integral
-            expV = _N.exp(_N.dot(B.T[i0:i1], aS) + _N.dot(Gm.T[0:i1-i0], phiS))
+            expV = _N.exp(_N.dot(B.T[i0:i1], aS) + _N.dot(Gm.T[gt0:gt1], phiS))
 
             if doAl:
                 for j in xrange(nbs1):
@@ -66,12 +74,12 @@ def h_dL(a_phi, *args):
                 if i1 - i0 > TM:
                     Tunt = TM
                 for j in xrange(nbs1, nbs1+nbs2):
-                    dL[j] += -dt*_N.dot(Gm.T[0:Tunt, j-nbs1], expV[0:Tunt])
+                    dL[j] += -dt*_N.dot(Gm.T[gt0:Tunt, j-nbs1], expV[gt0:Tunt])
         if doAl:
             for j in xrange(nbs1):
-                dL[j] += _N.sum(B.T[sts[m][1:], j])   #  First spk is fake
+                dL[j] += _N.sum(B.T[sts[m][1:], j])   #  always use 1st real spk
         if doPh:
-            allISIs = sts[m][1:] - sts[m][0:-1]
+            allISIs = sts[m][frstSpk+1:] - sts[m][frstSpk:-1]
             shrtISIs= allISIs[_N.where(allISIs < TM)[0]]
             for j in xrange(nbs1, nbs1 + nbs2):
                 dL[j] += _N.sum(Gm.T[shrtISIs, j-nbs1])
@@ -80,7 +88,6 @@ def h_dL(a_phi, *args):
     return dL
 
 def h_d2L(a_phi, *args):
-    print "2**"
     nbs1 = args[0]       #  number basis splines PSTH
     nbs2 = args[1]       #  number basis splines hist
     M    = args[2]       #  number trials
@@ -91,6 +98,8 @@ def h_d2L(a_phi, *args):
     doAl = args[7]
     doPh = args[8]
     TM   = args[9]
+    dt   = args[10]
+    frstSpk = args[11]   #  Ignore first spike or not.  (0 or 1)
 
     aS   = a_phi[0:nbs1]
     phiS  = a_phi[nbs1:]
@@ -103,28 +112,32 @@ def h_d2L(a_phi, *args):
     ##  "DIAGONAL" elements   phiS
     for m in xrange(M):
         ITVS = len(itvs[m])
-        nSpks= ITVS - 1
 
-        for it in xrange(ITVS):    #  
+        for it in xrange(frstSpk, ITVS):    #  
             i0 = itvs[m][it][0]
             i1 = itvs[m][it][1]
-            expV = _N.exp(_N.dot(B.T[i0:i1], aS) + _N.dot(Gm.T[0:i1-i0], phiS))
+            Tunt = i1 - i0
+            if i1 - i0 > TM:
+                Tunt = TM
+
+            gt0= 0      #  
+            if i0 < 1:  #  reference to fake spike
+                gt0 = 0-i0
+            gt1= i1-i0
+            if i0 < 1:  #  reference to fake spike
+                i0 = 0  #  if there was a spike, i0==1
+
+            expV = _N.exp(_N.dot(B.T[i0:i1], aS) + _N.dot(Gm.T[gt0:gt1], phiS))
 
             if doAl:
                 for j in xrange(nbs1):
                     for k in xrange(j, nbs1):
                         d2L[j, k] += -dt*_N.dot((B.T[i0:i1, j] * B.T[i0:i1, k]), expV)
             if doAl and doPh:
-                Tunt = i1 - i0
-                if i1 - i0 > TM:
-                    Tunt = TM
                 for j in xrange(nbs1):
                     for k in xrange(nbs1, nbs1+nbs2):
                         d2L[j, k] += -dt*_N.dot((Gm.T[0:Tunt, k-nbs1] * B.T[i0:i0+Tunt, j]), expV[0:Tunt])
             if doPh:
-                Tunt = i1 - i0
-                if i1 - i0 > TM:
-                    Tunt = TM
                 for j in xrange(nbs1, nbs1 + nbs2):
                     for k in xrange(j, nbs1+nbs2):
                         d2L[j, k] += -dt*_N.dot((Gm.T[0:Tunt, j-nbs1] * Gm.T[0:Tunt, k-nbs1]), expV[0:Tunt])

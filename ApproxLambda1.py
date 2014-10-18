f61d97c3bb6c87d014d51ef1a7b7837cb68e637e
KensukeArai <kensuke.y.arai@gmail.com>
2014-10-18 14:45:52 -0400

    Removed some files

--- ApproxLambda1.py
+++
@@ -1,7 +0,0 @@
-#  ISI pctl
-pctl0 = _N.array((pctl.shape[0] + 1, 2))
-pctl0[0, 0] = 0
-pctl0[0, 1] = 0
-pctl0[1:, :] = pctl
-
-_N.interp()
--- postSmpx.py
+++
@@ -1,6 +0,0 @@
-map_smpx = _N.empty(_d.N + 1)
-for n in xrange(2, _d.N+1+2):
-    cts, vals = _N.histogram(Bsmpx[0, :, n], bins=50)
-    l = cts.tolist()
-    i = l.index(cts.max())
-    map_smpx[n-2] = 0.5*(vals[i] + vals[i+1])
